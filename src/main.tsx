040487cd973e245d312f2ee7493620cc10fff00b
fbngsk <132647343+fbngsk@users.noreply.github.com>
2025-11-30 18:32:25 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { registerServiceWorker } from './registerSW';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+// Service Worker registrieren
+registerServiceWorker();
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
