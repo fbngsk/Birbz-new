120ff315aa046910640cd63cbccecd9d966fc07d
fbngsk <132647343+fbngsk@users.noreply.github.com>
2025-12-05 10:43:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react'; 
 import { Header } from './components/Header';
 import { BottomNav } from './components/BottomNav';
 import { DailyHoroscope } from './components/DailyHoroscope';
